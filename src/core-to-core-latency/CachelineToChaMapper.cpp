a3bfc88c701e0f037a3e3100d9f4e4d79d8a1d08
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-04-21 19:21:16 +0200

    add read benchmark

--- src/core-to-core-latency/CachelineToChaMapper.cpp
+++ src/core-to-core-latency/CachelineToChaMapper.cpp
@@ -5,14 +5,35 @@
 
 namespace cclat {
 
-auto CachelineToChaMapper::run(void* Cachelines, std::size_t NumberOfCachelines) -> ChaToCachelinesMap {
+auto CachelineToChaMapper::run(void* Cachelines, std::size_t NumberOfCachelines,
+                               std::size_t NumberOfCachelineReads) -> ChaToCachelinesMap {
   ChaToCachelinesMap ChaToCachelines;
 
   for (auto CachelineIndex = 0; CachelineIndex < NumberOfCachelines; CachelineIndex++) {
     // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     auto* Cacheline = static_cast<uint8_t*>(Cachelines) + static_cast<ptrdiff_t>(64 * CachelineIndex);
 
-    // read, flush and repeat. the uncore counter for CHA reads will increment if this cacheline is in the counter.
+    // flush, read, flush and repeat. the uncore counter for CHA reads will increment if this cacheline is in the
+    // counter.
+
+    // TODO: start the counter on all CHAs
+
+    volatile uint8_t Sum = 0;
+    for (auto I = 0; I < NumberOfCachelineReads; I++) {
+      Sum += *Cacheline;
+      asm __volatile__("mfence\n"
+                       "lfence\n"
+                       "clflush [%[addr]]\n"
+                       "mfence\n"
+                       "lfence" ::[addr] "r"(Cacheline)
+                       : "memory");
+    }
+
+    (void)Sum;
+
+    // TODO: stop the counter on all CHAs
+
+    // TODO: find the CHA index where approximatly NumberOfCachelineReads occured.
 
     auto ChaIndex = 0;
 
